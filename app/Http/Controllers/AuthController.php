e30fd16d57e45c0ca602cacd12934712a1f518c0
AhmedSBelal <ahmadsd77777@gmail.com>
2025-01-31 17:42:34 +0200

    Authentication is done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function loginShow() {
+        return view('Auth.login');
+    }
+
+    public function login(LoginRequest $request) {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) { //$request->boolean('remember')
+            $request->session()->regenerate();
+            return redirect()->intended(route('dashboard'));
+        }
+        return back()->withErrors(['email' => 'The provided credentials do not match our records.']);
+    }
+
+    public function registerShow() {
+        return view('Auth.register');
+    }
+
+    public function register(RegisterRequest $request) {
+        $user = $request->validated();
+        $user['password'] = Hash::make($user['password']);
+        User::create($user);
+        return redirect()->route('login.show')->with('success', 'You are Registered successfully');
+    }
+
+    public function logout(Request $request) {
+        session()->flush();
+        Auth::logout();
+        return redirect()->route('login.show')->with('success', 'You are Logged out successfully');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('login', [\App\Http\Controllers\AuthController::class, 'loginShow'])->name('login.show');
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login'])->name('login.post');
+Route::get('register', [\App\Http\Controllers\AuthController::class, 'registerShow'])->name('register.show');
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register'])->name('register.post');
 
+Route::middleware(['auth'])->group(function () {
+
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+    Route::get('/', function () {
+        return view('Dashboard.index');
+    })->name('dashboard');
+
+});
